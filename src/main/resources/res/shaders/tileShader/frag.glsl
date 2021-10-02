caa1702886ca2e8864d5dc2b7d7bfeded5f6e713
C1FR1 <c1fr1.stkcps@gmail.com>
2021-10-02 12:16:27 +0000

    automated commit #57

--- src/main/resources/res/shaders/tileShader/frag.glsl
+++ src/main/resources/res/shaders/tileShader/frag.glsl
@@ -8,11 +8,18 @@ uniform float strength;
 uniform vec2 pos;
 uniform sampler2D texSampler;
 
+vec3 emberSlide(float val) {
+	return vec3(clamp(3 - val * 3, 0, 1), 1 - val, 0);
+}
+
 void main() {
 	color = texture(texSampler, tc);
 	vec2 realPos = pos;
 	realPos.x += tc.x;
 	realPos.y -= tc.y;
 	float noise = gnoise(vec4(2 * realPos, 0, 0)) / 2 + 0.5;
-	color.w *= noise;
+	if (noise > strength) {
+		color.w = 0;
+	}
+	color.xyz = mix(color.xyz, emberSlide((strength - noise) * 10), clamp((strength - noise) * 5, 0, 1));
 }
