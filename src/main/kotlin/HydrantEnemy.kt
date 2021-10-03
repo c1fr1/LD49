062a0fa49bc5f72999b7acf7f4486ebdc2712749
C1FR1 <c1fr1.stkcps@gmail.com>
2021-10-03 10:35:28 +0000

    automated commit #146

--- src/main/kotlin/HydrantEnemy.kt
+++ src/main/kotlin/HydrantEnemy.kt
@@ -1,11 +1,16 @@
+import engine.PIf
 import engine.entities.Orientation2D
 import org.joml.Vector2i
+import kotlin.math.PI
 
 class HydrantEnemy(x : Float, y : Float) : Enemy(x, y) {
 	override val bounty = 30
 
 	override fun shootProjectiles(projectileList : ArrayList<Projectile>, playerPos : Orientation2D) {
-		projectileList.add(LinearProjectile())
+		if (x > 0) {
+			rotation = -PIf
+		}
+		projectileList.add(LinearProjectile(this))
 	}
 
 	override fun protectsTile(worldPos : Vector2i, tx : Int, ty : Int) = ty >= worldPos.y - 1
