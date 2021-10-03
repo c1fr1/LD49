c4a63df52dce7bdfb66832176cc7f38266ddc587
C1FR1 <c1fr1.stkcps@gmail.com>
2021-10-03 15:36:06 +0000

    automated commit #166

--- src/main/kotlin/TutorialManager.kt
+++ src/main/kotlin/TutorialManager.kt
@@ -1,6 +1,15 @@
 class TutorialManager {
-
+	val step = 0
+	var texts = arrayOf(
+		arrayOf("WASD to move, left click to shoot"),
+		arrayOf("tiles burn when you stand of them"))
 	fun manage(world : World) {
-
+		if (step == 0) {
+			for (row in world.tiles) {
+				for (i in row.indices) {
+					row[i] = 1f
+				}
+			}
+		}
 	}
 }
